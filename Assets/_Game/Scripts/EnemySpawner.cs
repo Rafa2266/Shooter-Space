d0fbf3d7e7959a00231d5c32a980c576adee42ff
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2024-06-26 23:36:04 -0300

    Movimentação dos inimigos

--- Assets/_Game/Scripts/EnemySpawner.cs
+++ Assets/_Game/Scripts/EnemySpawner.cs
@@ -5,17 +5,74 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private float spawnDelay;
+    [SerializeField] private float spawnDelay, formationDistanceScreenX, movementSpeed;
+    private float minX, maxX;
+    private bool isMoveRight;
     // Start is called before the first frame update
     void Start()
     {
         SpawnUntilFull();
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveEnemies();
+    }
 
+    private void MoveEnemies()
+    {
+        if (isMoveRight)
+        {
+            transform.Translate(new Vector2(movementSpeed * Time.deltaTime,0f));
+        }
+        else
+        {
+            transform.Translate(new Vector2(-movementSpeed * Time.deltaTime, 0f));
+        }
+
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        if((transform.position.x - formationDistanceScreenX) < minX)
+        {
+            isMoveRight= true;
+        }
+
+        else if ((transform.position.x + formationDistanceScreenX) > maxX)
+        {
+            isMoveRight = false;
+        }
+    }
+
+    private void SetMinAndMaxWidth()
+    {
+        Vector2 screenDimensions = Camera.main.ScreenToWorldPoint(new Vector2(Screen.safeArea.width,Screen.safeArea.height));
+        minX= -screenDimensions.x;
+        maxX= screenDimensions.x;
+    }
+    private void Initialize()
+    {
+        SetRandomDirectionMovement();
+        SetMinAndMaxWidth();
+
+    }
+
+    private void SetRandomDirectionMovement()
+    {
+        int randomNumber= Random.Range(0, 2);
+        
+        if(randomNumber == 0 )
+        {
+            isMoveRight = false;
+        }
+        else
+        {
+            isMoveRight = true;
+        }
     }
 
     private void SpawnUntilFull()
