2bb88ed23ec58a71a865952dc309ba43eb03445c
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2024-07-09 22:51:34 -0300

    Disparo do projétil pelos inimigos

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -7,17 +7,34 @@ public class Enemy : MonoBehaviour
     public int health;
     private FlashEffect flashEffect;
     private Explosion explosion;
+
+    [SerializeField] private GameObject projectTile;
+    [SerializeField] private Transform spawnProjectTilePosition;
+    [SerializeField] private float startInvokeShoot,minShootValue,maxShootValue;
+
+
     // Start is called before the first frame update
     void Start()
     {
         flashEffect=this.GetComponent<FlashEffect>();
         explosion=FindObjectOfType<Explosion>();
+        InvokeProjectTile();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InvokeProjectTile()
+    {
+        InvokeRepeating("ShootProjectTile",startInvokeShoot, Random.Range(minShootValue,maxShootValue));
+    }
+
+    private void ShootProjectTile()
+    {
+        GameObject tempProjectTile = Instantiate(projectTile,spawnProjectTilePosition.position,Quaternion.identity) as GameObject;
     }
     private void OnTriggerEnter2D(Collider2D target)
     {
